5662ff7fe66afb5f0928a6b73d2a02fe4f2d208d
Alex Ford <alex@cratercreative.com>
2012-06-04 23:30:06 -0400

    Adding the actual files
    Also basically just testing github

---
+++ template/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<title>Page Title</title>
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<link rel="stylesheet" href="css/main.css" />
+
+	<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+	<link rel="shortcut icon" href="img/favicon.ico">
+	<link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="img/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="img/apple-touch-icon-114x114.png">
+
+</head>
+<body>
+	<div id='wrap'>
+		
+	<section id='main'>
+		
+			<div class="container">
+				<div class="sixteen columns">
+					<h1 class="remove-bottom" style="margin-top: 40px">Crater Skeleton</h1>
+					<h5>Based on Skeleton 1.1 (<a href='https://github.com/dhgamache/Skeleton'>Skeleton GitHub</a>)</h5>
+					<hr />
+				</div>
+				
+				<div class='one-third column'>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</div>
+		
+			</div><!-- container -->
+		</div>
+		
+	</div><!-- #main -->
+
+	<footer id='bottom'>
+		<div class='container'>
+			<div class='sixteen columns'>
+				<h2>Sticky Footer</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</div>
+		</div>
+	</footer><!-- #bottom -->
+	
+
+	<script src="http://code.jquery.com/jquery-1.7.2.min.js"></script>
+	<script src="/js/main.js"></script>
+</body>
+</html>
---
+++ template/js/main.js
@@ -0,0 +1,12 @@
+/*
+*	Crater Skeleton JS
+*	Adapted from Skeleton 1.1
+*	Created 5/10/2012
+*/
+
+$(function() {
+	// document ready
+	
+	
+	
+});
